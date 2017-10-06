d17964d3c5ca754719756855efc76d43a40b994c
bradcsweet <bradcsweet@gmail.com>
2017-10-06 13:50:44 -0400

    Part 1 and 2 of project

--- Lucky Numbers/Program.cs
+++ Lucky Numbers/Program.cs
@@ -10,6 +10,54 @@ namespace Lucky_Numbers
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("What is the lowest number in the number range?");
+            int lowRange = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is the highest number in the number range?");
+            int highRange = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Guess six numbers in that range that you think are the LUCKY NUMBERS");
+
+            int[] userNumbers = new int[6];
+            for (int i = 0; i < userNumbers.Length; i++)
+            {
+                Console.WriteLine("Enter a number");
+                int userNumber = int.Parse(Console.ReadLine());
+                while (userNumber < lowRange || userNumber > highRange)
+                {
+                    Console.WriteLine("Number is out of range and not valid. Enter a number between " + lowRange + " and " + highRange + ".");
+                    userNumber = int.Parse(Console.ReadLine());
+                }
+                userNumbers[i] = userNumber;
+            }
+
+
+            int[] luckyNumbers = new int[6];
+            Random randoNumber = new Random();
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                int luckyNumber = randoNumber.Next(lowRange, highRange + 1);
+                luckyNumbers[i] = luckyNumber;
+            }
+
+
+            foreach (int luckyNumber in luckyNumbers)
+            {
+                Console.WriteLine("Lucky Number: " + luckyNumber);
+            }
+
+            int jackpotTotal = 750000;
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
